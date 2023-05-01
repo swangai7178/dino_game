5b3009bc98bd9c3c1f5a98b0e9cf5663c801504c
samuel wangai <swangai7178@gmail.com>
2023-05-01 12:19:00 +0300

    update

--- lib/dino_player.dart
+++ lib/dino_player.dart
@@ -9,7 +9,7 @@ class DinoPlayer extends SpriteAnimationComponent with HasGameRef {
   late final SpriteAnimation _walkingLeftAnimation;
   late final SpriteAnimation _idleAnimation;
 
-  final double _animationSpeed = .15;
+  final double _animationSpeed = 5;
   Direction direction = Direction.none;
 
   @override
